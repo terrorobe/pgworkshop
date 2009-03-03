74e4ee0050fbd4ecbaddff9172dad8ce03d4f173
robe@localhost <robe@localhost>
2009-03-03 01:33:28 +0100

    perltidy

--- pgexerciser.pl
+++ pgexerciser.pl
@@ -20,35 +20,32 @@ my $dbpass;
 
 my $lorem = Text::Lorem->new();
 
-
 GetOptions(
-		'num-clients:i' => \$num_clients,
-		'create-schema' => \$create_schema,
-		'reset-schema' => \$reset_schema,
-		'database:s' => \$dbname,
-		'user:s' => \$dbuser,
-		'password:s' => \$dbpass,
-		'host:s' => \$dbhost,
-	  ) or pod2usage ( -verbose => 0 );
-
+    'num-clients:i' => \$num_clients,
+    'create-schema' => \$create_schema,
+    'reset-schema'  => \$reset_schema,
+    'database:s'    => \$dbname,
+    'user:s'        => \$dbuser,
+    'password:s'    => \$dbpass,
+    'host:s'        => \$dbhost,
+) or pod2usage( -verbose => 0 );
 
 $num_clients ||= 10;
-$dbname ||= 'sqlsim';
-$dbuser ||= '';
-$dbpass ||= '';
-
+$dbname      ||= 'sqlsim';
+$dbuser      ||= '';
+$dbpass      ||= '';
 
 if ($create_schema) {
 
-	create_schema();
+    create_schema();
 
-	exit(0);
+    exit(0);
 }
 
 if ($reset_schema) {
 
-	reset_schema();
-	exit(0);
+    reset_schema();
+    exit(0);
 }
 
 exercise_db();
@@ -56,213 +53,212 @@ exercise_db();
 print "done\n";
 exit(0);
 
-
-
 ########
 # Subs #
 ########
 
-
 sub exercise_db {
-	for my $client ( 1 .. $num_clients ) {
-		POE::Session->create(
-				inline_states => {
-				_start => sub {
-				$_[KERNEL]->yield( start_client => $client );
-				},
+    for my $client ( 1 .. $num_clients ) {
+        POE::Session->create(
+            inline_states => {
+                _start => sub {
+                    $_[KERNEL]->yield( start_client => $client );
+                },
 
-				start_client => \&start_client,
-				do_something => \&decide_next_action,
-				create_auction => \&create_auction,
-				place_bid => \&place_bid,
-				log_in => \&log_in,
-				log_out => \&log_out,
-				create_user => \&create_user,
+                start_client   => \&start_client,
+                do_something   => \&decide_next_action,
+                create_auction => \&create_auction,
+                place_bid      => \&place_bid,
+                log_in         => \&log_in,
+                log_out        => \&log_out,
+                create_user    => \&create_user,
 
-				},
-				);
-	}
-	POE::Kernel->run();
+            },
+        );
+    }
+    POE::Kernel->run();
 }
-
 
 sub reset_schema {
 
-	my $sqlreset = <<'HERE';
+    my $sqlreset = <<'HERE';
 TRUNCATE "user" CASCADE;
 ALTER SEQUENCE auction_id_seq RESTART 1;
 ALTER SEQUENCE bid_id_seq RESTART 1;
 ALTER SEQUENCE user_id_seq RESTART 1;
 HERE
-	my $dbh = get_dbh();
+    my $dbh = get_dbh();
 
-	$dbh->do($sqlreset);
-	$dbh->commit();
+    $dbh->do($sqlreset);
+    $dbh->commit();
 
 }
 
 sub get_dbh {
 
-	my $dsn = "dbi:Pg:dbname=$dbname";
-	$dsn .= ";host=$dbhost" if ($dbhost);
-	my $dbh = DBI->connect($dsn, $dbuser, $dbpass, {AutoCommit => 0, RaiseError => 1});
-	die("Failed to connect to db: $!\n") unless $dbh;
-	return $dbh;
+    my $dsn = "dbi:Pg:dbname=$dbname";
+    $dsn .= ";host=$dbhost" if ($dbhost);
+    my $dbh =
+      DBI->connect( $dsn, $dbuser, $dbpass,
+        { AutoCommit => 0, RaiseError => 1 } );
+    die("Failed to connect to db: $!\n") unless $dbh;
+    return $dbh;
 }
 
 sub start_client {
 
-	my $dbh = get_dbh();
+    my $dbh = get_dbh();
 
-	$_[HEAP]->{'dbh'} = $dbh;
-	$_[HEAP]->{'sid'} = $_[ARG0];
+    $_[HEAP]->{'dbh'} = $dbh;
+    $_[HEAP]->{'sid'} = $_[ARG0];
 
-	logit($_[HEAP], "Initialized db connection");
-	$_[KERNEL]->yield("do_something");
+    logit( $_[HEAP], "Initialized db connection" );
+    $_[KERNEL]->yield("do_something");
 }
-
 
 sub decide_next_action {
 
-	my $delay = int(rand(10));
+    my $delay = int( rand(10) );
 
-	my @actions = qw (create_auction log_out);
-	push @actions, ('place_bid') x 20;
-	my $action = $actions[rand @actions];
+    my @actions = qw (create_auction log_out);
+    push @actions, ('place_bid') x 20;
+    my $action = $actions[ rand @actions ];
 
-	if (! defined $_[HEAP]->{'user'}) {
-		$action = rand() > 0.75 ? 'create_user' : 'log_in';
-	}
+    if ( !defined $_[HEAP]->{'user'} ) {
+        $action = rand() > 0.75 ? 'create_user' : 'log_in';
+    }
 
-	$_[KERNEL]->delay($action => $delay);
-#	$_[KERNEL]->yield($action);
+    $_[KERNEL]->delay( $action => $delay );
+
+    #	$_[KERNEL]->yield($action);
 }
-
 
 sub create_auction {
 
-	my $sql = 'INSERT INTO auction (creator, description, current_bid, end_time) VALUES (?, ?, ?, ?)';
-	my $dbh = $_[HEAP]->{'dbh'};
-	my $sth = $dbh->prepare($sql);
+    my $sql =
+'INSERT INTO auction (creator, description, current_bid, end_time) VALUES (?, ?, ?, ?)';
+    my $dbh = $_[HEAP]->{'dbh'};
+    my $sth = $dbh->prepare($sql);
 
-	my $start_bid = int(rand(51));
-	my $expire_time = int(rand(11));
+    my $start_bid   = int( rand(51) );
+    my $expire_time = int( rand(11) );
 
-	# Hackhackhack
-	my $endtime = "NOW() + '$expire_time min'::interval";
-	my ($endtime) = $dbh->selectrow_array("SELECT $endtime");
+    # Hackhackhack
+    my $endtime = "NOW() + '$expire_time min'::interval";
+    my ($endtime) = $dbh->selectrow_array("SELECT $endtime");
 
-	$sth->execute($_[HEAP]->{'user'}, create_text(), $start_bid, $endtime);
-	my ($auctionid) = $dbh->last_insert_id(undef, undef, "auction", undef);
-	$dbh->commit();
+    $sth->execute( $_[HEAP]->{'user'}, create_text(), $start_bid, $endtime );
+    my ($auctionid) = $dbh->last_insert_id( undef, undef, "auction", undef );
+    $dbh->commit();
 
-	logit($_[HEAP], "Created auction #$auctionid");
-	$_[KERNEL]->yield("do_something");
+    logit( $_[HEAP], "Created auction #$auctionid" );
+    $_[KERNEL]->yield("do_something");
 }
 
 sub place_bid {
 
-	my $dbh = $_[HEAP]->{'dbh'};
+    my $dbh = $_[HEAP]->{'dbh'};
 
-	my $sql_auctions = q|SELECT id, current_bid FROM auction WHERE end_time > NOW() + '60 seconds'::interval|;
-	
-	my @auctions = @{ $dbh->selectall_arrayref($sql_auctions) };
+    my $sql_auctions =
+q|SELECT id, current_bid FROM auction WHERE end_time > NOW() + '60 seconds'::interval|;
 
-	# Create an auction if there are too few active auctions
+    my @auctions = @{ $dbh->selectall_arrayref($sql_auctions) };
 
-	if (! @auctions || @auctions < ($num_clients / 2)) {
-		$_[KERNEL]->yield("create_auction");
-		return;
-	}
+    # Create an auction if there are too few active auctions
 
-	my ($auction_id, $current_bid) = @{ $auctions[ rand @auctions ] };
+    if ( !@auctions || @auctions < ( $num_clients / 2 ) ) {
+        $_[KERNEL]->yield("create_auction");
+        return;
+    }
 
-	my $sql = 'INSERT INTO bid (bidder, auction, bid) VALUES (?, ?, ?)';
+    my ( $auction_id, $current_bid ) = @{ $auctions[ rand @auctions ] };
 
-	my $sth = $dbh->prepare($sql);
+    my $sql = 'INSERT INTO bid (bidder, auction, bid) VALUES (?, ?, ?)';
 
-	my $new_bid = sprintf("%.2f", $current_bid * (1 + rand() / 10) + 1);
+    my $sth = $dbh->prepare($sql);
 
-	$sth->execute($_[HEAP]->{'user'}, $auction_id, $new_bid);
+    my $new_bid = sprintf( "%.2f", $current_bid * ( 1 + rand() / 10 ) + 1 );
 
-	my ($bid_id) = $dbh->last_insert_id(undef, undef, "bid", undef);
+    $sth->execute( $_[HEAP]->{'user'}, $auction_id, $new_bid );
 
-	$dbh->commit();
+    my ($bid_id) = $dbh->last_insert_id( undef, undef, "bid", undef );
 
-	logit($_[HEAP], "Placed bid #$bid_id on auction $auction_id. Value: $new_bid");
+    $dbh->commit();
 
-	$_[KERNEL]->yield("do_something");
+    logit( $_[HEAP],
+        "Placed bid #$bid_id on auction $auction_id. Value: $new_bid" );
+
+    $_[KERNEL]->yield("do_something");
 }
 
 sub create_user {
 
-	my $dbh = $_[HEAP]->{'dbh'};
+    my $dbh = $_[HEAP]->{'dbh'};
 
-	my $sql_insert = 'INSERT INTO "user" (name) VALUES (?)';
+    my $sql_insert = 'INSERT INTO "user" (name) VALUES (?)';
 
-	my $sth = $dbh->prepare($sql_insert);
+    my $sth = $dbh->prepare($sql_insert);
 
-	$sth->execute($lorem->words(3));
-	my ($userid) = $dbh->last_insert_id(undef, undef, "user", undef);
-	$dbh->commit();
+    $sth->execute( $lorem->words(3) );
+    my ($userid) = $dbh->last_insert_id( undef, undef, "user", undef );
+    $dbh->commit();
 
-	logit($_[HEAP], "Created user $userid");
+    logit( $_[HEAP], "Created user $userid" );
 
-	$_[HEAP]->{'user'} = $userid;
+    $_[HEAP]->{'user'} = $userid;
 
-	$_[KERNEL]->yield("do_something");
+    $_[KERNEL]->yield("do_something");
 }
 
 sub log_in {
 
-	my $dbh = $_[HEAP]->{'dbh'};
+    my $dbh = $_[HEAP]->{'dbh'};
 
-	my $sql = 'SELECT id FROM "user"';
+    my $sql = 'SELECT id FROM "user"';
 
-	my @userids = @{ $dbh->selectcol_arrayref($sql) };
+    my @userids = @{ $dbh->selectcol_arrayref($sql) };
 
-	if (@userids && @userids >= $num_clients) {
+    if ( @userids && @userids >= $num_clients ) {
 
-		my $user = $_[HEAP]->{'user'} = $userids[ rand @userids ];
+        my $user = $_[HEAP]->{'user'} = $userids[ rand @userids ];
 
-		logit($_[HEAP], "Logged in user $user");
+        logit( $_[HEAP], "Logged in user $user" );
 
-		$_[KERNEL]->yield("do_something");
-	}
-	else {
-		$_[KERNEL]->yield("create_user");
-	}
+        $_[KERNEL]->yield("do_something");
+    }
+    else {
+        $_[KERNEL]->yield("create_user");
+    }
 }
 
 sub log_out {
 
-	logit($_[HEAP], "Logged out user " . $_[HEAP]->{'user'});
+    logit( $_[HEAP], "Logged out user " . $_[HEAP]->{'user'} );
 
-	delete($_[HEAP]->{'user'});
+    delete( $_[HEAP]->{'user'} );
 
-	$_[KERNEL]->yield("do_something");
+    $_[KERNEL]->yield("do_something");
 }
 
 sub create_text {
 
-	# $lorem->words(5);
-	# $lorem->sentences(5);
-	# $lorem->paragraphs(5);
+    # $lorem->words(5);
+    # $lorem->sentences(5);
+    # $lorem->paragraphs(5);
 
-	return $lorem->paragraphs(1);
+    return $lorem->paragraphs(1);
 }
 
 sub logit {
 
-	my ($heap, $text) = @_;
+    my ( $heap, $text ) = @_;
 
-	printf "%3d: %s\n", $heap->{'sid'}, $text;
+    printf "%3d: %s\n", $heap->{'sid'}, $text;
 }
-
 
 sub create_schema {
 
-	my $schema = <<'HERE';
+    my $schema = <<'HERE';
 DROP LANGUAGE IF EXISTS plpgsql CASCADE;
 CREATE PROCEDURAL LANGUAGE plpgsql;
 
@@ -390,8 +386,8 @@ ALTER TABLE ONLY bid
     ADD CONSTRAINT bid_bidder_fkey FOREIGN KEY (bidder) REFERENCES "user"(id);
 HERE
 
-	my $dbh = get_dbh();
+    my $dbh = get_dbh();
 
-	$dbh->do($schema);
-	$dbh->commit();
+    $dbh->do($schema);
+    $dbh->commit();
 }
