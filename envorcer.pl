a73daa9033f43a8065da4682f3e222fb5da761f8
robe@trottelkunde.amd.co.at <robe@trottelkunde.amd.co.at>
2009-04-30 13:21:52 +0200

    more fixes

--- envorcer.pl
+++ envorcer.pl
@@ -196,7 +196,7 @@ sub create_logship {
 
 sub create_walmgr {
 
-        run_command('mkdir /srv/walmgr-data', 'slave');
+        run_command('su postgres -c "mkdir /srv/walmgr-data"', 'slave');
         run_command('rsync -avH /root/pgworkshop/ root@slave1:/root/pgworkshop', 'master');
         run_command('chown postgres:postgres /var/lib/postgresql/8.3', 'slave');
         run_command('apt-get -y -qq install psycopg2', 'both');
